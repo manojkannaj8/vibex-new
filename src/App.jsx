49730ed4c071b6980b5f1addb0087c5dfa712888
manojkannaj8 <manojkannaj8@gmail.com>
2026-02-07 14:19:14 +0530

    Update Supabase URL to placeholder
    Replaced the Supabase URL with a placeholder.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import { createClient } from '@supabase/supabase-js';
 
 // ⚠️ PASTE YOUR SUPABASE KEYS HERE
-const supabaseUrl = 'https://gicdnsjbjgguvqmkldmd.supabase.co';
+const supabaseUrl = 'url';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpY2Ruc2piamdndXZxbWtsZG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NDU4MDQsImV4cCI6MjA4NjAyMTgwNH0.to-UhSotsQQMPDte9r6broE1SRVX1zfm7C1CvV-v6HE';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
